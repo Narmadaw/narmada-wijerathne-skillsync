d8773dbea2bf6278fe5a32330c5ce94df7b29667
Narmadaw <narmadaw9@gmail.com>
2023-11-26 23:10:39 -0800

    styles

--- skillsync-client/src/App.js
+++ skillsync-client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
-import './App.css';
+import './App.scss';
 import Header from "./components/Header/Header";
 
 import HomePage from './pages/HomePage/HomePage';
