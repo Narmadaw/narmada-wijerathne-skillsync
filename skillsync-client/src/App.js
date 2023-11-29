7d2b517f2124e4170129ed7d242f8d8fa27a0041
Narmadaw <narmadaw9@gmail.com>
2023-11-29 01:54:25 -0800

    add styles

--- skillsync-client/src/App.js
+++ skillsync-client/src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import './App.scss';
 import Header from "./components/Header/Header";
-
 import HomePage from './pages/HomePage/HomePage';
 import UserProfile from "./components/UserProfile/UserProfile";
 import SelectedJob from "./components/SelectedJob/SelectedJob";
 import SearchJob from "./components/SearchJob/SearchJob";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
@@ -14,15 +14,20 @@ function App() {
   return (
     
       <BrowserRouter>
+      <div className="app">
         <Header />
-        <Routes>
+        <main>
+          <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/profile" element={<UserProfile />}/>
             <Route path="/:id/:title/:company/" element={<SelectedJob />}/>
             <Route path='/search' element={<SearchJob />}/>
           </Routes>
-
-      
+        </main>
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
       </BrowserRouter>
     
   );
--- skillsync-client/src/components/UserProfile/UserProfile.jsx
+++ skillsync-client/src/components/UserProfile/UserProfile.jsx
@@ -2,50 +2,66 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { Link } from 'react-router-dom';
+import BotImage from './../../assets/images/bot.png';
 import './UserProfile.scss';
 
 
 const UserProfile = () => {
-  const [file, setFile] = useState(null);
-  const [personalData, setPersonalData] = useState([]);
+  const [file, setFile] = useState();
+  const [personalData, setPersonalData] = useState({});
   const [experience, setExperience] = useState([]);
   const [skills, setSkills] = useState([]);
   const [education, setEducation] = useState([]);
   const [certificate, setCertificate] = useState([]);
+  const [name, setName] = useState({});
+  const [profession, setProfession] = useState('');
+  const [address, setAddress] = useState({});
+  const [selfSummary, setSelfSummary] = useState('');
+  const [objective, setObjective] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState(null);
+  const [phones, setPhones] = useState([]);
+  const [mails, setMails] = useState([]);
+  const [urls, setUrls] = useState([]);
   const [resumeRecords, setResumeRecords] = useState(
     JSON.parse(localStorage.getItem('resumeRecords')) || []
   );
 
+
   useEffect(() => {
     const storedRecords = JSON.parse(localStorage.getItem('resumeRecords'));
     if (storedRecords) {
-      setPersonalData(storedRecords.personal_infos)
+      setPersonalData(storedRecords.personal_infos);
       setExperience(storedRecords.work_experience.entries);
       setEducation(storedRecords.education.entries);
       setCertificate(storedRecords.certifications);
       setSkills(storedRecords.skills);
-      
+      setName(storedRecords.personal_infos.name || {});
+      setProfession(storedRecords.personal_infos.current_profession || '');
+      setAddress(storedRecords.personal_infos.address || {});
+      setSelfSummary(storedRecords.personal_infos.self_summary || '');
+      setObjective(storedRecords.personal_infos.objective || '');
+      setDateOfBirth(storedRecords.personal_infos.date_of_birth || null);
+      setPhones(storedRecords.personal_infos.phones || [0]);
+      setMails(storedRecords.personal_infos.mails || [0]);
+      setUrls(storedRecords.personal_infos.urls || []);
+      console.log(storedRecords);
     }
-  }, []);
+  }, []); 
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
    useEffect(() => {
-    if(!resumeRecords){
-      handleUpload();
+    if(!file){
+      console.error('No file selected');
     }
     else{
+      handleUpload();
     }
-    
   }, [file]);
 
   const handleUpload = async () => {
-    if (!file) {
-      console.error('No file selected');
-      return;
-    }
-
+  
     const formData = new FormData();
     formData.append('show_original_response', 'false');
     formData.append('fallback_providers', '');
@@ -65,7 +81,7 @@ const UserProfile = () => {
     try {
       const response = await axios(options);
       const extractedData = response.data.affinda.extracted_data;
-        localStorage.setItem('resumeRecords', JSON.stringify(extractedData));
+      localStorage.setItem('resumeRecords', JSON.stringify(extractedData));
         setPersonalData(extractedData.personal_infos);
         setExperience(extractedData.work_experience.entries);
         setEducation(extractedData.education.entries);
@@ -90,105 +106,118 @@ const UserProfile = () => {
   return (
     <>
     <div className="profile-wrapper">
-           <div className="upload-container">
-             <div className='upload-container__card'>
-               <CloudUploadIcon color="primary" fontSize="large"/> 
-             </div>
-             <div className='upload-container__card'>
-               <h1>Upload your resume to get started</h1>
-             </div>
-             <div className='upload-container__card'>
-               <input className='upload-container__btn-upload' type="file" accept=".pdf, .doc, .docx" onChange={handleFileChange} />
-             </div>
-         <div className='upload-container__btn-pannel'>
-       
-         <Link to={'/search'}>
-             <button className='upload-container__btn-search'>Search Jobs</button>         </Link>
-         </div>
-        
-         </div>
+      <div className="upload-container">
+        <div className='upload-container__card'>
+          <CloudUploadIcon color="primary" fontSize="large"/>
+        </div>
+        <div className='upload-container__card'>
+          <h1 className='upload-container__title'>Upload your resume to get started</h1>
+        </div>
+        <div className='upload-container__card'>
+          <input className='upload-container__btn-upload' type="file" accept=".pdf, .doc, .docx" onChange={handleFileChange} />
+          <p className='upload-container__text'>as .pdf | .doc | .docx</p>
+        </div>
+        <img className='upload-container__image' src={BotImage} alt="bot image" />
+      </div>
+    </div>
 
-         <div className='resume-container'>
+    <div className='resume'>
+      {/* <h3>Your Resume</h3> */}
+      <div className='resume-container'>
+        <div className="resume-container__left">
+          <h2>{name.raw_name}</h2>
+          <p>{profession}</p>
+
           <div>
-            <h2>Personal Details</h2>
-            
-            {/* <h2>{personalData.name.raw_name}</h2> */}
-            {/* <ul>
-            {personalData?.map((item, index) => (
-              <li key={index}>{item.name}</li>
-            ))}
-            </ul> */}
+            <p>{mails}</p>
+            <p>{phones}</p>
+            <ul className='resume-container__skill-list'>
+              {urls.map((item, key) => (
+                <li
+                  className='resume-container__skill-list-item'
+                  key={key}>
+                  {item}
+                </li>
+              ))}
+            </ul>
           </div>
+
           <div>
-            <h2>Work Experience</h2>
-            <ul>
-                {experience.map((item, index) => {
+          <h4 className='resume-container__sub-title'>PROFILE</h4>
+            <p>{selfSummary}</p>
+          </div>
+
+          <div>
+            <h4 className='resume-container__sub-title'>SKILLS</h4>
+            <ul className='resume-container__skill-list'>
+            {skills.map((item, index) => {
                 return (
-                  <li key={index}>
-                    
-                    <p>
-                    {item.title}
-                    </p>
+                  <li 
+                  className='resume-container__skill-list-item'
+                  key={index}>{item.name}</li>
+                );
+              })}
+            </ul>
+          </div>
 
-                    <p>
-                    {item.start_date} - {item.end_date}
-                    </p>
-
-                    <p>
-                    {item.company} 
-                    </p>
+        </div>
+        <div className='resume-container__v-line'></div>
+        <div className="resume-container__right">
+          <div>
+            <h4 className='resume-container__sub-title'>EXPERIENCE</h4>
+            <hr className='resume-container__h-line'/>
+            <ul className='resume-container__skill-list'>
+            {experience.map((item, index) => {
+              return (
+              <li key={index}
+              className='resume-container__skill-list-item'>
+                <p className='resume-container__sub'>{item.title} | {item.company} </p>
+                <p>{item.start_date} - {item.end_date}</p>
                   </li>
                 );
               })}
             </ul>
           </div>
           <div>
-            <h2>Education</h2>
-            <ul>
+            <h4 className='resume-container__sub-title'>EDUCATION</h4>
+            <hr className='resume-container__h-line'/>
+            <ul className='resume-container__skill-list'>
             {education.map((item, index) => {
                     return (
-                      <li key={index}>
-                        <p>{item.accreditation}</p>
+                      <li key={index}
+                          className='resume-container__skill-list-item'>
+                        <p className='resume-container__sub'>{item.establishment} | {item.accreditation}</p>
                         <p>{item.start_date} - {item.end_date}</p>
-                        <p>{item.establishment}</p>
-
-                        {/* Use map to render each location */}
                         <p>
-                          {/* {item.location?.map((loc, index) => {
+                          {education.location?.map((loc, index) => {
                             return (
                               <p key={index}>
                                 {loc.city} - {loc.country}
                               </p>
                             );
-                          })} */}
+                          })}
                         </p>
                       </li>
                     );
                   })}
             </ul>
           </div>
+
           <div>
-            <h2>Certification</h2>
-            <ul>
-                {certificate.map((item, index) => {
-                return (
-                  <li key={index}>{item.name}</li>
-                );
-              })}
+            <h4 className='resume-container__sub-title'>PROJECTS</h4>
+            <hr className='resume-container__h-line'/>
+            <ul className='resume-container__skill-list'>
+              <li className='resume-container__skill-list-item'></li>
             </ul>
           </div>
-          <div>
-            <h2>Skills</h2>
-            <ul>
-                          {skills.map((item, index) => {
-                return (
-                  <li key={index}>{item.name}</li>
-                );
-              })}
-            </ul>
-          </div>
-         </div>
+         
+
+        </div>
+
+      </div>
+
     </div>
+
     </>
   );
 };
